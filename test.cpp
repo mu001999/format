f2236c5339cab8865ee2e3e364f93ba7a241613b
mu001999 <mu001999@outlook.com>
2020-12-16 15:23:09 +0800

    update test

--- test.cpp
+++ test.cpp
@@ -4,5 +4,5 @@
 int main()
 {
     std::cout << fmt::format<"%d %s">::fmt(42, "Mu00") << std::endl;
-    std::cout << fmt::format<"%d">::fmt("test") << std::endl;
+    // std::cout << fmt::format<"%d">::fmt("test") << std::endl; // error
 }
